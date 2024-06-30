3e8750f9eb98dd5a8b600fca3c57562dc4c762ff
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-06-30 10:10:19 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+from os import getenv
+
+from os import getenv
+
+API_ID = getenv("API_ID", "")
+API_HASH = getenv("API_HASH", "")
+
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_USERNAME = getenv("BOT_USERNAME", "")
+
+MONGO_DB_URL = getenv("MONGO_DB_URL", "")
+DB_NAME = getenv("DB_NAME", "kerang")
+
+FORCE_SUB_CHANNEL = getenv("FORCE_SUB_CHANNEL", "")
+FORCE_SUB_GROUP = getenv("FORCE_SUB_GROUP", "")
+
+BROADCAST_AS_COPY = True

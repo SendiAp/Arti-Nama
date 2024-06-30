58042b64939af60326275c83c39009a54be8acc3
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-06-30 09:46:50 +0700

    Create nama.py

---
+++ nama.py
@@ -0,0 +1,11 @@
+Arti = [
+  "Analitis, memahami, pengetahuan, senang belajar, bermeditasi, penuh kesadaran.",
+  "Pemrakarsa, pelopor, pemimpin, bebas, pekerja keras, individualis.",
+  "Mudah bekerja sama, beradaptasi, bermitra, memperhatikan orang lain, penengah.",
+  "Ekspresif, mudah berbicara, bersosialisasi, seni dan menikmati hidup.",
+  "Tingkat spiritual tinggi, intuitif, tercerahkan, idealis, pemimpi.",
+  "Mengutamakan prinsip, keteraturan, pelayanan, sulit menerima batasan dan perkembangan yang mapan.",
+  "Bertanggung jawab, melindungi, merawat, bermasyarakat, seimbang, simpatik.",
+  "Ekspansif, visioner, petualang, menggunakan kebebasan dengan cara konstruktif.",
+  "Peduli sesama, dermawan, tidak mementingkan diri sendiri, patuh terhadap kewajiban, ekspresi kreatif."
+]

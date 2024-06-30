371dd90c911eb35800818f8853cded26a5b27cd2
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-06-30 11:15:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -37,6 +37,28 @@ def get_arg(message: Message):
         return ""
     return " ".join(split[1:])
 
+# Date and time
+def dt():
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M")
+    dt_list = dt_string.split(" ")
+    return dt_list
+
+
+def dt_tom():
+    a = (
+        str(int(dt()[0].split("/")[0]) + 1)
+        + "/"
+        + dt()[0].split("/")[1]
+        + "/"
+        + dt()[0].split("/")[2]
+    )
+    return a
+
+
+today = str(dt()[0])
+tomorrow = str(dt_tom())
+
 START_TEXT = """
 Halo {} , Saya akan mengartikan namamu
 
